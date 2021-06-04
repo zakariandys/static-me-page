6e88ff1c0bba8842676833235ccfb31e4b9e99f3
zakaria.saputro <zakaria.saputro@dana.id>
2021-06-05 01:29:08 +0700

    fix typo on metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,7 +21,7 @@ module.exports = {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: 'Zakaria Andy Saputro',
-        short_name: 'Zakara',
+        short_name: 'Zakaria',
         start_url: '/',
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
